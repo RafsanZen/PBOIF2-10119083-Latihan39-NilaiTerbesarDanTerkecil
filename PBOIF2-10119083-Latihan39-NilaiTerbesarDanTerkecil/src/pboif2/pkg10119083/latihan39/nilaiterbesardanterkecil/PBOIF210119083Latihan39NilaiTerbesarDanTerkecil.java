17ac2ccb75db021f9c5d6ced772923ccdfe38454
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-06 21:56:30 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan39-NilaiTerbesarDanTerkecil/src/pboif2/pkg10119083/latihan39/nilaiterbesardanterkecil/PBOIF210119083Latihan39NilaiTerbesarDanTerkecil.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan39.nilaiterbesardanterkecil;
+
+import java.util.Scanner;
+
+/**
+ *
+ *Nama : Rafsan Zen Mustaofa 
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Program Nilai Terbesar dan Terkecil
+ */
+public class PBOIF210119083Latihan39NilaiTerbesarDanTerkecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        nilaiMahasiswa nilaiMhs = new nilaiMahasiswa();
+        Petugas petugas = new Petugas();
+        
+        System.out.println("=====Program Nilai Terbesar dan Terkecil "
+                + "Nilai Mahasiswa=====");
+  
+        // input petugas
+        System.out.print("Masukkan Nama Petugas : ");
+        petugas.setNama(scanner.next());
+        
+        // input banyak mahasiswa
+        System.out.print("Masukkan Banyaknya Nilai Mahasiswa : ");
+        nilaiMhs.setJmlMahasiswa(scanner.nextInt());
+        
+        // input nilai per mahasiswa
+        for(int i = 0; i < nilaiMhs.getJmlMahasiswa(); i++) {
+            System.out.print("Masukkan Nilai Mahasiswa Ke-"+(i+1)+" = ");
+            nilaiMhs.setNilai(scanner.nextInt());
+        }
+        
+        nilaiMhs.tampilNilai();
+        System.out.println("\nPetugas : " + petugas.getNama());
+        
+         System.out.println("Developed by : Rafsan Zen Mustaofa");
+    }
+    
+}
---
+++ PBOIF2-10119083-Latihan39-NilaiTerbesarDanTerkecil/src/pboif2/pkg10119083/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * Nama : Rafsan Zen Mustaofa 
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Program Petugas
+ */
+public class Petugas {
+      private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
